0602937088f31a75e6cf01170201c662926b80e6
Michael Pope <mpope.cr@gmail.com>
2015-01-13 14:54:48 -0500

    Make display hello world test pass

--- app/src/androidTest/java/com/example/pope4president/espressoexample/DisplayHelloWorldTest.java
+++ app/src/androidTest/java/com/example/pope4president/espressoexample/DisplayHelloWorldTest.java
@@ -2,6 +2,11 @@ package com.example.pope4president.espressoexample;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 public class DisplayHelloWorldTest extends ActivityInstrumentationTestCase2<MainActivity> {
     public DisplayHelloWorldTest() {
         super(MainActivity.class);
